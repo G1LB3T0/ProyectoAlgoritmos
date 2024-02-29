231702476ee26d703650d952b7ce2f24ab6eb6ef
Carlos <alb231311@uvg.edu.gt>
2024-02-29 10:42:45 -0600

    Update Arithmetic_operations

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,11 +1,13 @@
 public class ArithmeticOperations {
     public double execute(String operator, double operand1, double operand2) throws Exception {
-        return switch (operator) {
-            case "+" -> operand1 + operand2;
-            case "-" -> operand1 - operand2;
-            case "*" -> operand1 * operand2;
-            case "/" -> operand1 / operand2;
-            default -> throw new IllegalArgumentException("Unknown operator: " + operator);
-        };
+        switch (operator) {
+            case "+": return operand1 + operand2;
+            case "-": return operand1 - operand2;
+            case "*": return operand1 * operand2;
+            case "/": return operand1 / operand2;
+            case "<": return operand1 < operand2 ? 1 : 0; // Retorna 1 para verdadero, 0 para falso
+            case ">": return operand1 > operand2 ? 1 : 0; // Retorna 1 para verdadero, 0 para falso
+            default: throw new IllegalArgumentException("Unknown operator: " + operator);
+        }
     }
 }
